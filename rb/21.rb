18a4e4b77ea239de42effbbc942df3a3f2d182c1
3ifbyw <3ifbyw@gmail.com>
2014-01-29 14:03:39 -0500

    Calculate sum directly

--- rb/21.rb
+++ rb/21.rb
@@ -2,11 +2,11 @@ def divsum n
   (1..n / 2).select { |m| n % m == 0 }.reduce(:+)
 end
 
-amicable = []
+sum = 0
 
 (2..10000).each do |i| # 220
   ds = divsum i
-  amicable << i if i == divsum(ds) && i != ds
+  sum += i if i == divsum(ds) && i != ds
 end
 
-p amicable.reduce(:+)
+p sum
