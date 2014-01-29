2116f75b3d2a2494f2c6d01bedd90bba250c8399
3ifbyw <3ifbyw@gmail.com>
2014-01-29 11:46:54 -0500

    Use prime standard library

--- rb/49.rb
+++ rb/49.rb
@@ -1,6 +1,4 @@
-def prime? n
-  ([2] + [*3..Math.sqrt(n)]).map { |c| n % c }.all? { |m| m > 0 }
-end
+require 'prime'
 
 def same_digits? ns
   ns.map { |n| n.to_s.chars.sort }.uniq.size == 1
@@ -8,7 +6,7 @@ end
 
 def solution? n
   ns = [n, n + 3330, n + 6660]
-  return ns.all? { |n| prime? n } && same_digits?(ns) ? ns : nil
+  return ns.all?(&:prime?) && same_digits?(ns) ? ns : nil
 end
 
 x = 1488
