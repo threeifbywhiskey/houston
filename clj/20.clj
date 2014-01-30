09fd691bbe1ff4f3e25331ec582854788127e990
3ifbyw <3ifbyw@gmail.com>
2014-01-30 15:51:27 -0500

    Use digsum function from #16 solution

--- clj/20.clj
+++ clj/20.clj
@@ -1,3 +1,6 @@
-(prn (apply +
-  (map #(-  (int  (first %)) 48)
-    (re-seq #"\d" (str (apply * (range 2N 100)))))))
+(defn digsum [n]
+  (apply +
+         (map #(- (int (first %)) 48)
+              (re-seq #"\d" (str n)))))
+
+(prn (digsum (apply * (range 2N 100))))
