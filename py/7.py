a64e5450a741b6f2c7ee20ae7f3e84b962a3e133
3ifbyw <3ifbyw@gmail.com>
2014-01-29 20:15:34 -0500

    Improve #7 Python solution

--- py/7.py
+++ py/7.py
@@ -1,17 +1,18 @@
 from math import sqrt
 
 def prime(n):
-    if n == 1:
-        return False
     if n == 2:
         return True
 
-    cands = [2] + range(3, int(sqrt(n)) + 1)
+    if n % 2 == 0:
+        return False
+
+    cands = range(3, int(sqrt(n)) + 1)
     return all(map(lambda m: n % m, cands))
 
 count = 0
 
-for i in xrange(105000):
+for i in xrange(2, 105000):
     if prime(i):
         count += 1
     if (count == 10001):
