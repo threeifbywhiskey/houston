c4724c5ecf2bbd00b4977b5208fe39da90e2dfcc
3ifbyw <3ifbyw@gmail.com>
2014-01-31 22:19:26 -0500

    Add #59 input file and Ruby solution

---
+++ rb/59.rb
@@ -0,0 +1,13 @@
+cipher = eval '[' + IO.read('59') + ']'
+alpha  = [*97..122]
+keys   = alpha.product alpha, alpha
+
+key = keys.max_by do |key|
+  copy = cipher.dup
+  copy.size.times do |i|
+    copy[i] ^= key[i % 3]
+  end
+  copy.count 32
+end
+
+p cipher.zip(key.cycle).map { |a, b| a ^ b }.reduce(:+)
