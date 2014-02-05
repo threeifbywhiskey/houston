2d74e0b71083576d74a02085ae8233611979f42d
Three If By Whiskey <3ifbyw@gmail.com>
2014-02-05 16:14:10 -0500

    Add #22 Clojure solution

---
+++ clj/22.clj
@@ -0,0 +1,9 @@
+(def names
+  (sort (read-string (str "(" (slurp "houston/input/22") ")"))))
+
+(defn value [name]
+  (apply + (map #(- (int %) 64) name)))
+
+(prn (apply +
+            (for [n (range (count names))]
+              (* (+ 1 n) (value (nth names n))))))
