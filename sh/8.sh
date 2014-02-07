55e0980c476350401b21902ff283a7b32b3ba0c8
Three If By Whiskey <3ifbyw@gmail.com>
2014-02-06 19:10:07 -0500

    Improve #8 Shell solution

--- sh/8.sh
+++ sh/8.sh
@@ -1,10 +1,6 @@
 nums=`paste -sd '' ../input/8`
 
-for i in `seq 996`; do
-    prod=`echo $nums | cut -c $i`
-    prod=$((prod * `echo $nums | cut -c $((i + 1))`))
-    prod=$((prod * `echo $nums | cut -c $((i + 2))`))
-    prod=$((prod * `echo $nums | cut -c $((i + 3))`))
-    prod=$((prod * `echo $nums | cut -c $((i + 4))`))
-    echo $prod
+for i in `seq 5 996`; do
+    echo -n $nums | tail -c $i | head -c 5 |
+    sed 's/\B/\n/g' | paste -sd '*' | bc
 done | sort -n | tail -n 1
