3137abc6a195d45f45af3661d68e4deea59d74b3
Three If By Whiskey <3ifbyw@gmail.com>
2014-02-07 12:38:25 -0500

    Add #99 JavaScript solution

---
+++ js/99.js
@@ -0,0 +1,16 @@
+var fs = require('fs'),
+    input = fs.readFileSync('input/99'),
+    nums = input.toString().split('\n');
+
+var max = 0, line;
+
+for (var i = 0; i < nums.length; ++i) {
+    sp = nums[i].split(',');
+    a = sp[0] * 1, b = sp[1] * 1;
+
+    prod = Math.log(a) * b;
+    if (prod > max)
+        max = prod, line = i + 1;
+}
+
+console.log(line);
