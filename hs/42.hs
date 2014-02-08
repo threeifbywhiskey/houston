73cfa8e8f41709c826489961dea37220d0e7cb35
Three If By Whiskey <3ifbyw@gmail.com>
2014-02-08 10:55:59 -0500

    Add #42 Haskell solution

---
+++ hs/42.hs
@@ -0,0 +1,12 @@
+import Data.Char (ord)
+
+triangular n = foldl1 (+) [1..n]
+
+triangles n = map triangular [1..n]
+
+value = foldl1 (+) . map (\c -> ord c - 64)
+
+main = do
+    input <- readFile "houston/input/42"
+    let words = (read ("[" ++ input ++ "]") :: [String])
+    print $ length $ filter (\n -> elem n $ triangles 20) $ map value words
